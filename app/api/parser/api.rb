faabd014694bd5cd4fdf52c6ad264c9ab2847fcb
Sam Liu <samliuforwork@gmail.com>
2022-08-13 10:21:41 +0800

    api return user all posts

--- app/api/parser/api.rb
+++ app/api/parser/api.rb
@@ -3,17 +3,34 @@ module Parser
     version 'v1', using: :header, vendor: 'parser'
     format :json
     prefix :api
+    add_swagger_documentation
 
     resource :ptt do
       desc 'get random post from ptt'
       params do
-        optional :board, type: String
+        requires :board, type: String
         optional :page, type: Integer
-        optional :type, type: String
-        optional :query, type: Integer
+        requires :type, type: String, values: %w[recommend author thread]
+        requires :query, type: String
       end
       get do
         PostParser.new.search(params)
+      end
+    end
+
+    resource :users do
+      desc 'Return certain user all ptt post in given board'
+      params do
+        requires :board, type: String
+        optional :type, type: String, default: 'author'
+        requires :query, type: String
+      end
+      route_param :query do
+        resource :posts do
+          get do
+            PostParser.new.posts(params)
+          end
+        end
       end
     end
   end
--- app/models/post_parser.rb
+++ app/models/post_parser.rb
@@ -4,10 +4,10 @@ require 'uri'
 require 'net/http'
 
 class PostParser
-  def search(params)
-    post_urls = doc_node(params).search('div .title a').map { |url_path| ParseHost.ptt.delete_suffix('/bbs') + url_path.values[0] }
+  include ActionView::Helpers::SanitizeHelper
 
-    url = URI(post_urls.sample)
+  def search(params)
+    url = URI(urls(doc_node(params)).sample)
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
     request = Net::HTTP::Get.new(url)
@@ -15,11 +15,36 @@ class PostParser
     response.read_body
   end
 
+  def posts(params)
+    posts = []
+    doc_node(params).search('.r-ent').each_with_object(posts) do |node, post|
+      post << json(node)
+    end
+  end
+
   private
 
   def doc_node(params)
     url = "#{ParseHost.ptt}/#{params[:board]}/search?page=#{params[:page]}&q=#{params[:type]}%3A#{params[:query]}"
     Nokogiri::HTML(URI.parse(url).open)
+  end
+
+  def json(node)
+    {
+      date: node.search('.date').children.text,
+      recommends: node.search('.nrec').children.text.to_i,
+      title: node.search('.title a').children.text,
+      author: node.search('.author').children.text,
+      url: node.search('.title a')[0].values[0],
+      search_title: node.search('.dropdown .item a')[0].values,
+      search_author: node.search('.dropdown .item a')[1].values
+    }
+  end
+
+  def urls(node)
+    node.search('div .title a').map do |url_path|
+      ParseHost.ptt.delete_suffix('/bbs') + url_path.values[0]
+    end
   end
 
   # def last_page_value
